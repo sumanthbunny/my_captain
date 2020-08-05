6b29e2fbe9643ff0a1ffbe6914e244f625699ec8
sumanthbunny <vsumanth67@gmail.com>
2020-08-05 17:18:05 +0530

    Add files via upload

---
+++ possitive_integers_in_a_list.py
@@ -0,0 +1,9 @@
+l = []
+n = int(input("enter no. of inputs"))
+for x in range (0 , n , 1) :
+    ele=int(input())
+    l.append(ele)
+print("required list is:")
+for x in l :
+    if x > 0 :
+        print(x)
